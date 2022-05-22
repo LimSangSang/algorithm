03e4a71067ae5ab60cc9a566020528fd4eba73fd
seula0421@gmail.com <seula@limseula.local>
2022-05-22 14:39:44 +0900

    2022.05.22 k번째 큰 수

--- inflearn_python_algorithm/section2/section2.py
+++ inflearn_python_algorithm/section2/section2.py
@@ -16,7 +16,7 @@ k번 째 약수
 첫째 줄에 N의 약수들 중 K번째로 작은 수를 출력한다. 만일 N의 약수의 개수가 K개보다 적어서 K번째 약수가 존재하지 않을 경우에는 -1을 출력하시오.
 ▣ 입력예제 1 63
 ▣ 출력예제 1 3
-'''
+
 
 # 나의 풀이
 n, k = map(int, input().split())
@@ -43,7 +43,7 @@ for i in range(1, n+1):
         break
 else:
     print(-1)
-
+'''
 '''
 k번째 수
 
@@ -60,7 +60,7 @@ N개의 숫자로 이루어진 숫자열이 주어지면 해당 숫자열중에�
 ▣ 출력예제 1 
 #1 7
 #2 6
-'''
+
 
 #나의 풀이
 t = int(input())
@@ -73,7 +73,52 @@ for i in range(1, t+1):
     temp.sort()
 
     print(i, temp[k-1])
-    
-    
+'''
+
+'''
+k번째 큰 수
+
+현수는 1부터 100사이의 자연수가 적힌 N장의 카드를 가지고 있습니다. 같은 숫자의 카드가 여러장 있을 수 있습니다. 현수는 이 중 3장을 뽑아 각 카드에 적힌 수를 합한 값을 기록하려 고 합니다. 3장을 뽑을 수 있는 모든 경우를 기록합니다. 기록한 값 중 K번째로 큰 수를 출력 하는 프로그램을 작성하세요.
+만약 큰 수부터 만들어진 수가 25 25 23 23 22 20 19......이고 K값이 3이라면 K번째 큰 값 은 22입니다.
+▣ 입력설명
+첫 줄에 자연수 N(3<=N<=100)과 K(1<=K<=50) 입력되고, 그 다음 줄에 N개의 카드값이 입력 된다.
+▣ 출력설명
+첫 줄에 K번째 수를 출력합니다. K번째 수는 반드시 존재합니다.
+▣ 입력예제 1
+10 3
+13 15 34 23 45 65 33 11 26 42
+▣ 출력예제 1 143
+'''
+# 내 풀이
+# 중복 제거를 틀리게 구현
+n, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+total=[]
+for ai, a in enumerate(arr):
+    print('a > ', a, ai)
+    for bi, b in enumerate(arr):
+        if (ai != bi):
+            print('b > ', b, bi)
+        for ci, c in enumerate(arr):
+            if (ai != ci or bi != ci):
+                print('c > ', c, ci)
+                total.append(a+b+c)
+
+total.sort(reverse=True)
+print(total[k-1])
+
+# 해설
+n, k = map(int, input().split())
+a = list(map(int, input().split()))
+res = set()
+for i in range(n):
+    for j in range(i+1, n):
+        for m in range(j+1, n):
+            res.add(a[i]+a[j]+a[m])
+res=list(res)
+res.sort(reverse=True)
+print(res[k-1])
+
 
     
